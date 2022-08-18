c82241e77d90147e605dd579e551296762b6e110
Willow Syme <106210870+WillowSyme@users.noreply.github.com>
2022-08-18 04:15:42 +0000

    Got main.py to print out the start up menu - made some changes to how it displays

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ def start_up():
     name = input("To begin, please enter your name: ")
     print("Thank you, {}.".format(name))
     print("Your journey will begin shortly.")
+    print("\n")
     print("You will be presented with 10 scenarios, each with 3 options for you to choose from.")
     print("You need to decide which option to choose.")
     print("Choose wisely, as your answers will determine if you are fit to be an Environmental Activist, or if you are doomed for all eternity.")
 
 def main():
+    start_up()
     print("Hello, world!")
 
 main()
