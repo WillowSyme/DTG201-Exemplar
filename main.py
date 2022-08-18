27089f0ff86630a1a2801fcfc1a7ef5b75462b9f
Willow Syme <106210870+WillowSyme@users.noreply.github.com>
2022-08-18 04:07:32 +0000

    Created folders for all the scenarios and endings, started functionality of the start up menu in main

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def start_up():
+    print("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~")
+    print("~ Welcome To: Environmental Activists ~")
+    print("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~")
+
+    name = input("To begin, please enter your name: ")
+    print("Thank you, {}.".format(name))
+    print("Your journey will begin shortly.")
+    print("You will be presented with 10 scenarios, each with 3 options for you to choose from.")
+    print("You need to decide which option to choose.")
+    print("Choose wisely, as your answers will determine if you are fit to be an Environmental Activist, or if you are doomed for all eternity.")
+
 def main():
     print("Hello, world!")
 
