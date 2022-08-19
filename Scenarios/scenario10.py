c3435ef1c55f0b10219ed3a9147685952f1287a1
Willow Syme <106210870+WillowSyme@users.noreply.github.com>
2022-08-19 04:17:36 +0000

    Completed setting up dictionaries for all of my scenarios!

--- Scenarios/scenario10.py
+++ Scenarios/scenario10.py
@@ -1,7 +1,7 @@
 scenario10 = {
-    "scenario" : "character is doing something",
-    "answers" : [],
-    "best_answer" : "",
-    "neutral_answer" : "",
-    "negative_answer" : ""
+    "scenario" : "Stacy has been challenging herself to No Waste November. It’s nearing the end of the month and she has been doing such a great job! Her best friend's birthday is coming up on December 1st and she realises she’s getting really low on wrapping paper. She’s not sure if she has enough to cover the gift she’s gotten. Her friend is an avid sewer and Stacy has some leftover fabric from a project she tried out months ago. She’s also been doodling a lot lately. What would be the best option Stacy should go for to wrap her friends gift? Use the leftover fabric - reusable wrapping paper is a cool thing now! Doodle on some regular printer paper and stick it together for a unique wrapping paper. Just go out and buy a new roll of paper. Christmas is coming up anyway, and she’ll need the extra for that.",
+    "answers" : ["a: Doodle on some printer paper", "b: Buy the new roll", "c: fabric all the way!"],
+    "best_answer" : "c",
+    "neutral_answer" : "a",
+    "negative_answer" : "b"
 }
